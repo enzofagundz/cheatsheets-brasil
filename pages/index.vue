b4d914c1b53d98b22437dda0d7bd4c9e7b2a46bd
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-01-12 20:26:14 -0300

    feat: enhance index page with dynamic document links and async data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
-<script setup>
-
-</script>
-
 <template>
   <TheHero />
+  <div class="bg-base-100 w-screen flex flex-row space-x-4 justify-center pt-8">
+    <NuxtLink v-for="doc in docs" :key="doc._path" :to="doc._path"
+      class="btn btn-primary">
+      {{ doc.title }}
+    </NuxtLink>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+const { data: docs } = await useAsyncData('docs', () =>
+  queryContent()
+  .find()
+)
+</script>
 
-</style>
+<style scoped></style>
