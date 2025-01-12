e00526212e0a4b246c21de7439b8b09bbd7667bf
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-01-12 19:29:21 -0300

    feat: add TheHero and TheNavbar components for layout and navigation

---
+++ components/TheHero.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="hero">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+            <div>
+                <h1 class="text-5xl font-bold">Box Office News!</h1>
+                <p class="py-6">
+                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar">
+        <div class="flex-1">
+            <NuxtLink :to="{ name: 'index' }" class="btn btn-ghost text-xl">CheatSheets Brasil</NuxtLink>
+        </div>
+        <div class="flex-none gap-2">
+            <div class="form-control">
+                <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+            </div>
+        </div>
+    </div>
+</template>
