58456e15fdd2360963b281b2701d8dcaeb0e853b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2025-01-12 19:29:15 -0300

    feat: add content module with markdown configuration and emoji support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  css: ['~/assets/css/main.css'],
+  compatibilityDate: "2024-11-01",
+  css: ["~/assets/css/main.css"],
   devtools: { enabled: true },
+
   postcss: {
     plugins: {
       tailwindcss: {},
-      autoprefixer: {}
-    }
-  }
-})
+      autoprefixer: {},
+    },
+  },
+  content: {
+    markdown: {
+      remarkPlugins: {
+        "remark-emoji": {
+          emoticon: true,
+        },
+      },
+      anchorLinks: true,
+      toc: {
+        depth: 3,
+        searchDepth: 3,
+      },
+    },
+  },
+  modules: ["@nuxt/content"],
+});
